ac07d29add084ee83104984b238fab8e874e2d01
박성경 <sungkyung@itsdcode.com>
2021-06-23 15:20:50 +0900

    1

--- common.js
+++ common.js
@@ -5,21 +5,21 @@ function includeJS(jsFilePath) {
   js.src = jsFilePath;
 
   document.body.appendChild(js);
+  console.log('includeJS')
 }
 
 function includeCss(cssFilePath) {
   var css = document.createElement('link');
   css.type = 'stylesheet';
   css.src = cssFilePath;
+  console.log('includeCss')
 }
 
-
-
-$(document).ready(function () {
-  includeJS('https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js');
+includeJS('https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js');
 includeJS('https://cdnjs.cloudflare.com/ajax/libs/bxslider/4.2.15/jquery.bxslider.min.js');
 includeCss('https://cdnjs.cloudflare.com/ajax/libs/bxslider/4.2.15/jquery.bxslider.min.css');
-  
+
+$(document).ready(function () {
   alert();
   $('#wrap .bxslider').bxSlider({
     auto: true,
