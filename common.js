aeca4e432ca93680850a5953b933bcacbf196c13
박성경 <sungkyung@itsdcode.com>
2021-06-23 17:55:21 +0900

    -

--- common.js
+++ common.js
@@ -1,40 +1,45 @@
 // @ts-ignore
 
+
 function includeJS(jsFilePath) {
   return new Promise(resolve => {
     var js = document.createElement('script');
     js.type = 'text/javascript';
     js.src = jsFilePath;
-
     const r = document.head.appendChild(js);
-    console.log('includeJS')
-    resolve(r);
-  })
+
+    js.onload = function() {
+      resolve(r);
+    };
+  });
 }
 
 function includeCss(cssFilePath) {
   return new Promise(resolve => {
-  var css = document.createElement('link');
-  css.type = 'stylesheet';
-  css.src = cssFilePath;
-
+    var css = document.createElement('link');
+    css.type = 'stylesheet';
+    css.src = cssFilePath;
     const r = document.head.appendChild(css);
-  console.log('includeCss')
+
     resolve(r);
-  })
+  });
 }
 
-
-
-$(document).ready(async function() {
-  console.log('ready 1')
-
+async function importJs(){
+  await includeCss('https://cdnjs.cloudflare.com/ajax/libs/bxslider/4.2.15/jquery.bxslider.min.css');
   await includeJS('https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js');
   await includeJS('https://cdnjs.cloudflare.com/ajax/libs/bxslider/4.2.15/jquery.bxslider.min.js');
-  await includeCss('https://cdnjs.cloudflare.com/ajax/libs/bxslider/4.2.15/jquery.bxslider.min.css');
 
+  commonJs();
+}
+
+$(document).ready(function() {
+  importJs();
+});
+
+function commonJs(){
   $('#wrap .bxslider').bxSlider({
     auto: true,
   });
-  console.log('ready 2')
-});
+}
+
